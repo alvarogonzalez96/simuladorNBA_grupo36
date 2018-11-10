99a04b69f923d47956569a31c5ca1b6813d01dee
alvaro <alvaro@192.168.1.59>
2018-11-10 12:13:50 +0100

    PlayOffs

--- NBA/src/negocio/Liga.java
+++ NBA/src/negocio/Liga.java
@@ -3,6 +3,7 @@ package negocio;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Map;
 
 import javax.swing.SwingUtilities;
 
@@ -24,7 +25,7 @@ public class Liga {
 	protected Equipo[] equipos;
 	protected ArrayList<Jugador> jugadores;
 	protected ArrayList<Jugador> agentesLibres;
-	protected HashMap<String, Clasificacion> clasificaciones;
+	protected static HashMap<String, Clasificacion> clasificaciones;
 	
 	protected boolean recienCreada;
 	protected int anyo;
@@ -200,6 +201,95 @@ public class Liga {
 		System.out.println("-----------------------------------------");
 		System.out.println("PREMIOS DE LA TEMPORADA REGULAR");
 		System.out.println("MVP: " + elegirMVP(l.equipos).getNombre());
+		
+		//PlayOffs
+		System.out.println();
+		System.out.println("-----------------------------------------");
+		System.out.println("PLAYOFFS");
+		playOffs(clasificaciones);
+	}
+	
+	public static void playOffs(HashMap<String, Clasificacion> clasificaciones) {
+		Equipo[] oeste = new Equipo[8];
+		Equipo[] este = new Equipo[8];
+		
+		Clasificacion cOeste = clasificaciones.get("OESTE");
+		Clasificacion cEste = clasificaciones.get("ESTE");
+		
+		for (int i = 0; i < oeste.length; i++) {
+			oeste[i] = cOeste.get(i);
+			este[i] = cEste.get(i);
+		}
+
+		//Cuartos de final de conferencia
+		Equipo[] semisOeste = new Equipo[4];
+		Equipo[] semisEste = new Equipo[4];
+		System.out.println();
+		System.out.println("*CUARTOS DE FINAL DE CONFERENCIA*");
+		int j = 7;
+		for (int i = 0; i < semisOeste.length; i++) {
+			semisOeste[i] = ganadorSerie(oeste[i], oeste[j]);
+			System.out.println("Gana la serie: " + semisOeste[i].getNombre());
+			System.out.println();
+			semisEste[i] = ganadorSerie(este[i], este[j]);
+			System.out.println("Gana la serie: " + semisEste[i].getNombre());	
+			System.out.println();
+			j--;
+		}
+		
+		//Semifinales de conferencia
+		oeste = new Equipo[2];
+		este = new Equipo[2];
+		j = 3;
+		System.out.println();
+		System.out.println("*SEMIFINALES DE CONFERENCIA*");
+		for (int i = 0; i < oeste.length; i++) {
+			oeste[i] = ganadorSerie(semisOeste[i], semisOeste[j]);
+			System.out.println("Gana la serie: " + oeste[i].getNombre());
+			System.out.println();
+			este[i] = ganadorSerie(semisEste[i], semisEste[j]);
+			System.out.println("Gana la serie: " + semisEste[i].getNombre());	
+			System.out.println();
+			j--;
+		}
+		
+		//Final de conferencia
+		Equipo[] equiposFinal = new Equipo[2];
+		System.out.println();
+		System.out.println("*FINALES DE CONFERENCIA*");
+		equiposFinal[0] = ganadorSerie(oeste[0], oeste[1]);
+		System.out.println("Gana la serie: " + equiposFinal[0].getNombre());
+		System.out.println();
+		equiposFinal[1] = ganadorSerie(este[0], este[1]);
+		System.out.println("Gana la serie: " + equiposFinal[1].getNombre());
+		System.out.println();
+		
+		//Final
+		System.out.println("*FINAL DE LA NBA*");
+		Equipo ganadorNBA = ganadorSerie(equiposFinal[0], equiposFinal[1]);
+		System.out.println("Gana el anillo: " + ganadorNBA.getNombre());
+		
+	}
+	
+	public static Equipo ganadorSerie(Equipo e1, Equipo e2) {
+		int victorias1 = 0;
+		int victorias2 = 0;
+		
+		while(victorias1 < 4 && victorias2 < 4) {
+			Partido p = new Partido(e1, e2);
+			p.jugar();
+			if(p.puntosLocal > p.puntosVisitante) {
+				victorias1 ++;
+			} else {
+				victorias2 ++;
+			}
+		}
+		
+		if(victorias1 > victorias2) {
+			return e1;
+		} else {
+			return e2;
+		}
 		
 	}
 	
