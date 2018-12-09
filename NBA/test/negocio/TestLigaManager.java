823f108d1970335336b6918055f5b14fe6b2b711
alvaro <alvaro@192.168.1.59>
2018-12-09 19:35:21 +0100

    JUnit

---
+++ NBA/test/negocio/TestLigaManager.java
@@ -0,0 +1,54 @@
+package negocio;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestLigaManager {
+
+	Jugador j1;
+	Equipo e1;
+	ArrayList<Jugador> jugadores;
+	
+	@Before
+	public void setUp() throws Exception {
+		j1 = new Jugador();
+		jugadores = new ArrayList<Jugador>();
+		
+		for (int i = 0; i < 11; i++) {
+			jugadores.add(new Jugador());
+		}
+		e1 = new Equipo(30, jugadores);
+		e1.jugadores = jugadores;
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testValoracionJugador() {
+		assertEquals(0, j1.getValoracion(), 0.1);
+	}
+	
+	@Test
+	public void testEquipoSalario() {
+		e1.calcSalarioTotal();
+		assertEquals(0, e1.salarioTotal, 0.1);
+		
+		int i = 1;
+		for (Jugador j : jugadores) {
+			j.salario = i;
+			i++;
+		}
+		e1.calcSalarioTotal();
+		assertEquals(66, e1.salarioTotal, 0.1);
+	}
+	
+	
+
+}
