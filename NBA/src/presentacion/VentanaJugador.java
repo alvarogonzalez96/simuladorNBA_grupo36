2b8e5b608a80d5679625839e86a27f1f10b6f9aa
alvaro <alvaro@192.168.1.59>
2018-12-30 13:42:12 +0100

    sincro

--- NBA/src/presentacion/VentanaJugador.java
+++ NBA/src/presentacion/VentanaJugador.java
@@ -200,7 +200,6 @@ public class VentanaJugador extends JFrame {
 			double rand = Math.random();
 			double randAnyos = Math.random()*5;
 			if(j.getOverall() >= 85 && cantidad >= 30000 + rand*10001  && anyos >= randAnyos) {
-
 				return 1;
 			} 
 			if(j.getOverall() >= 80 && j.getOverall() < 85 && cantidad >= 20000 + rand*10001 && anyos >= randAnyos) {
@@ -210,29 +209,12 @@ public class VentanaJugador extends JFrame {
 				return 1;
 			} 
 			if(j.getOverall() >= 60 && j.getOverall() < 70 && cantidad >= 1000 + rand*12001) {
-
 				return 1;
-
 			}
 			if(j.getOverall() < 60 && cantidad >= 1000 + rand*4001) {
 				return 1;
-			} 	
-
-			
-			if(j.getOverall() >= 80 && j.getOverall() < 85 && cantidad >= 20000 + rand*10001 && anyos >= randAnyos) {
-				return 1;
-			} 
-			if(j.getOverall() >= 70 && j.getOverall() < 80 && cantidad >= 10000+rand*10001 && anyos >= randAnyos-1) {
-				return 1;
-			} 
-			if(j.getOverall() >= 60 && j.getOverall() < 70 && cantidad >= 1000 + rand*12001) {
-				return 1;	 				
-			}	 			
-			if(j.getOverall() < 60 && cantidad >= 1000 + rand*4001) {
-				return 1;
-			} 	
+			} 		
 			return 0;
-
 		}
 	}
 	
